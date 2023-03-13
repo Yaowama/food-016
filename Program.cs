122d93524f3a2ea873b89b3207abf05ba2a6b8ef
Yaowaman <Yaowaman.0889170931@gmail.com>
2023-03-13 17:29:45 +0700

    yaowaman

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace mdt112 {
+    class Program {
+        static void Main (string[] args){
+            Console.Write("Day of Week(1-7): ");
+            int day = int.Parse (Console.ReadLine());
+            if(day == 1 ){
+            int monday = 1 ;
+            Console.WriteLine("{0} = Monday ", day, monday);
+            }
+            if(day == 2 ){
+            int tuesday = 2 ;
+            Console.WriteLine("{0} = Tuesday ", day, tuesday);
+            }
+            if(day == 3 ){
+            int wednesday = 3 ;
+            Console.WriteLine("{0} = Wednesday ", day, wednesday);
+            }
+            if(day == 4 ){
+            int thursday = 4 ;
+            Console.WriteLine("{0} = Thursday ", day, thursday);
+            }
+            if(day == 5 ){
+            int friday = 5 ;
+            Console.WriteLine("{0} = Friday ", day, friday);
+            }
+            if(day == 6 ){
+            int saterday = 6 ;
+            Console.WriteLine("{0} = Saterday ", day, saterday);
+            }
+            if(day == 7 ){
+            int sunday = 7 ;
+            Console.WriteLine("{0} = Sunday ", day, sunday);
+            }
+
+            Console.Write("Time of Day: ");
+            double time = double.Parse (Console.ReadLine());
+            if( time == 8 ){
+            double time8 = 8.00 ;
+            Console.WriteLine("{0} = 8.00 ", time, time8);
+            }
+            if( time == 9 ){
+            double time9 = 9.00 ;
+            Console.WriteLine("{0} = 9.00 ", time, time9);
+            }
+            if( time == 10 ){
+            double time10 = 10.00 ;
+            Console.WriteLine("{0} = 10.00 ", time, time10);
+            }
+             if( time == 11 ){
+            double time11 = 11.00 ;
+            Console.WriteLine("{0} = 11.00 ", time, time11);
+            }
+             if( time == 12 ){
+            double time12 = 12.00 ;
+            Console.WriteLine("{0} = 12.00 ", time, time12);
+            }
+             if( time == 13 ){
+            double time13 = 13.00 ;
+            Console.WriteLine("{0} = 13.00 ", time, time13);
+            }
+             if( time == 14 ){
+            double time14 = 14.00 ;
+            Console.WriteLine("{0} = 14.00 ", time, time14);
+            }
+             if( time == 15 ){
+            double time15 = 15.00 ;
+            Console.WriteLine("{0} = 15.00 ", time, time15);
+            }
+             if( time == 16 ){
+            double time16 = 16.00 ;
+            Console.WriteLine("{0} = 16.00 ", time, time16);
+            }
+             if( time == 17 ){
+            double time17 = 17.00 ;
+            Console.WriteLine("{0} = 17.00 ", time, time17);
+            }
+             if( time == 18 ){
+            double time18 = 18.00 ;
+            Console.WriteLine("{0} = 18.00 ", time, time18);
+            }
+        }
+    }
+}
+
